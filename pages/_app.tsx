dc5058c4ae31c34ac9a6dc80868e8551dbb37834
dev-goutham <dev.gouthamram95@gmail.com>
2022-01-12 01:35:53 +0530

    base layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,14 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import theme from "@/lib/theme";
+import Layout from "frontend/components/Layout";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 };
